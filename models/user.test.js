535af1c7f46408f0dbd8b50f9fa3fc7e24d324de
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-07-11 15:45:41 -0700

    adding applyToJob method in user model

--- models/user.test.js
+++ models/user.test.js
@@ -214,14 +214,49 @@ describe("update", function () {
 describe("remove", function () {
   test("works", async function () {
     await User.remove("u1");
-    const res = await db.query(
-        "SELECT * FROM users WHERE username='u1'");
+    const res = await db.query("SELECT * FROM users WHERE username='u1'");
     expect(res.rows.length).toEqual(0);
   });
 
   test("not found if no such user", async function () {
     try {
       await User.remove("nope");
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+});
+
+/************************************** applyToJob */
+
+describe("applyToJob", function () {
+  test("works", async function () {
+    await User.applyToJob("u1", testJobIds[1]);
+
+    const res = await db.query("SELECT * FROM applications WHERE job_id=$1", [
+      testJobIds[1],
+    ]);
+    expect(res.rows).toEqual([
+      {
+        job_id: testJobIds[1],
+        username: "u1",
+      },
+    ]);
+  });
+
+  test("not found if no such job", async function () {
+    try {
+      await User.applyToJob("u1", 0, "applied");
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+
+  test("not found if no such user", async function () {
+    try {
+      await User.applyToJob("nope", testJobIds[0], "applied");
       fail();
     } catch (err) {
       expect(err instanceof NotFoundError).toBeTruthy();
